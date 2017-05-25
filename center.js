07d163f9ce246d9765e478f6f98bf71196b6bea1
Tino-F <tinofileccia@yahoo.com>
2017-05-25 03:15:44 +0000

    vanilla

---
+++ center.js
@@ -0,0 +1,17 @@
+"use strict";
+let div, margin_top;
+
+window.onload = () => {
+
+  div = document.getElementById( 'center' );
+  margin_top = window.innerHeight - div.offsetHeight;
+  div.style.marginTop = ( ( margin_top / 2 ) - 10 ) + 'px';
+
+};
+
+window.onresize = () => {
+
+  margin_top = window.innerHeight - div.offsetHeight;
+  div.style.marginTop = ( ( margin_top / 2 ) - 10 ) + 'px';
+
+};
